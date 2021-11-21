08e65bfa3ca17468675cd4ba664d67cb264d4e53
hemanthkandasamy <94169511+hemanthkandasamy@users.noreply.github.com>
2021-11-21 21:07:16 +0530

    Update operations.c

--- 3_Implementation/Src/operations.c
+++ 3_Implementation/Src/operations.c
@@ -1,1 +1,42 @@
 
+* Booking  stages  with 4 stages
+*
+*/
+include<stdio.h>
+#include<stdlib.h>
+#include"moviedetails.c"
+#include"decleration.c" 
+#include<strings.h>
+
+/**
+* Selection of seat row and movie price
+*/
+
+int **seat,choice,price=200;
+
+/**
+*Reservation for paticular seat using calloc
+*/
+  
+void reservation(int *array,int price,int selection
+
+/**
+ *cancel the ticket and Unreserved
+ */
+  
+void cancel(int *array)
+  
+/**
+Detials of the canceled Ticket
+*/
+                 
+void details(void)
+                 
+/**
+Detials of the Movie and return
+*/                 
+  
+int movie(void)
+
+                 
+#return
